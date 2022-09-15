a4d964da7de6a230ddca8dae5fe42764560365f8
Elivandro <Elivandro@hotmail.com.br>
2022-09-14 21:00:27 -0400

    Imoveis migration added

---
+++ database/migrations/2022_09_15_004326_create_imoveis_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('imoveis', function (Blueprint $table) {
+            $table->id();
+            $table->string('description');
+            $table->string('address');
+            $table->string('neighborhood');
+            $table->integer('number');
+            $table->string('postal_code');
+            $table->string('city');
+            $table->string('country');
+            $table->integer('price');
+            $table->integer('roomQty');
+            $table->enum('type', ['apartament', 'house', 'kitnet']);
+            $table->enum('purpose', ['sale', 'location']);
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('imoveis');
+    }
+};
