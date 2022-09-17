2caeacca30eb75f6b46e5279d8faf4343edff6b7
Elivandro <Elivandro@hotmail.com.br>
2022-09-16 20:04:26 -0400

    create view error messages added

--- resources/views/imoveis/create.blade.php
+++ resources/views/imoveis/create.blade.php
@@ -5,7 +5,10 @@
     <section>
         <div class="card card-default">
             <div class="card-header">
-                <h3>Cadastre um Imóvel</h3>
+                <ol class="breadcrumb m-0 p-0">
+                    <li class="breadcrumb-item" aria-current="page"><a href="{{ route('imovel.index') }}">Imoveis</a></li>
+                    <li class="breadcrumb-item active">Adicionar</li>
+                </ol>
             </div>
             <div class="card-body">
                 <form method="post" action="{{ route('imovel.store') }}">
@@ -14,19 +17,34 @@
                     <hr/>
                     <div class="form-group mb-2">
                         <label for="description">Descrição</label>
-                        <input type="text" class="form-control" id="description" name="description" placeholder="Descrição" required/>
+                        <input type="text" class="form-control {{ $errors->has('description') ? 'border border-danger' : 'border border-primary' }}" id="description" name="description" placeholder="Descrição" required/>
+                        @if($errors->has('description'))
+                            <span class="text-danger">
+                                <strong>{{ $errors->first('description') }}</strong>
+                            </span>
+                        @endif
                     </div>
                     <div class="row mb-2">
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label for="price">Preço</label>
-                                <input type="text" class="form-control" id="price" name="price" placeholder="Preço" pattern="\d*" required/>
+                                <input type="text" class="form-control  {{ $errors->has('price') ? 'border border-danger' : 'border border-primary' }}" id="price" name="price" placeholder="Preço" pattern="\d*" required/>
+                                @if($errors->has('price'))
+                                    <span class="text-danger">
+                                        <strong>{{ $errors->first('price') }}</strong>
+                                    </span>
+                                @endif
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label for="roomqty">Quantidade de quartos</label>
-                                <input type="number" class="form-control" id="roomqty" name="roomQty" placeholder="Quantidade de quartos" min="1" required/>
+                                <input type="number" class="form-control  {{ $errors->has('roomQty') ? 'border border-danger' : 'border border-primary' }}" id="roomqty" name="roomQty" placeholder="Quantidade de quartos" min="1" required/>
+                                @if($errors->has('roomQty'))
+                                    <span class="text-danger">
+                                        <strong>{{ $errors->first('roomQty') }}</strong>
+                                    </span>
+                                @endif
                             </div>
                         </div>
                     </div>
@@ -34,22 +52,33 @@
                         <div class="col-md-6">
                             <div class="form-group mb-2">
                                 <label for="type">Tipo de imóvel</label>
-                                <select class="form-control" id="type" name="type" required>
+                                <select class="form-control {{ $errors->has('roomQty') ? 'border border-danger' : 'border border-primary' }}" id="type" name="type" required>
                                     <option>Selecione</option>
-                                    <option value="0">Apartamento</option>
-                                    <option value="1">Casa</option>
-                                    <option value="2">Kitnet</option>
+                                    <option value="1">Apartamento</option>
+                                    <option value="2">Casa</option>
+                                    <option value="3">Kitnet</option>
+                                    <option value="4">Lote</option>
                                 </select>
+                                @if($errors->has('type'))
+                                    <span class="text-danger">
+                                        <strong>{{ $errors->first('type') }}</strong>
+                                    </span>
+                                @endif
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group mb-2">
                                 <label for="purpose">Propósito</label>
-                                <select class="form-control" id="purpose" name="purpose" required>
+                                <select class="form-control {{ $errors->has('purpose') ? 'border border-danger' : 'border border-primary' }}" id="purpose" name="purpose" required>
                                     <option>Selecione</option>
-                                    <option value="0">Venda</option>
-                                    <option value="1">Locação</option>
+                                    <option value="1">Venda</option>
+                                    <option value="2">Locação</option>
                                 </select>
+                                @if($errors->has('purpose'))
+                                    <span class="text-danger">
+                                        <strong>{{ $errors->first('purpose') }}</strong>
+                                    </span>
+                                @endif
                             </div>
                         </div>
                     </div>
@@ -59,13 +88,23 @@
                         <div class="col-md-10">
                         <div class="form-group mb-2">
                             <label for="address">Endereço</label>
-                            <input type="text" class="form-control" id="address" name="address" placeholder="Logradouro" required/>
+                            <input type="text" class="form-control {{ $errors->has('address') ? 'border border-danger' : 'border border-primary' }}" id="address" name="address" placeholder="Logradouro" required/>
+                            @if($errors->has('address'))
+                                    <span class="text-danger">
+                                        <strong>{{ $errors->first('address') }}</strong>
+                                    </span>
+                            @endif
                         </div>
                         </div>
                         <div class="col-md-2">
                             <div class="form-group mb-2">
                                 <label for="number">Número</label>
-                                <input type="text" class="form-control" id="number=" name="number" placeholder="Número" pattern="\d*" required/>
+                                <input type="text" class="form-control {{ $errors->has('number') ? 'border border-danger' : 'border border-primary' }}" id="number=" name="number" placeholder="Número" pattern="\d*" required/>
+                                @if($errors->has('number'))
+                                    <span class="text-danger">
+                                        <strong>{{ $errors->first('number') }}</strong>
+                                    </span>
+                                @endif
                             </div>
                         </div>
                     </div>
@@ -73,13 +112,23 @@
                         <div class="col-md-8">
                             <div class="form-group mb-2">
                                 <label for="neighborhood">Bairro</label>
-                                <input type="text" class="form-control" id="neighborhood" name="neighborhood" placeholder="Bairro" required/>
+                                <input type="text" class="form-control {{ $errors->has('neighborhood') ? 'border border-danger' : 'border border-primary' }}" id="neighborhood" name="neighborhood" placeholder="Bairro" required/>
+                                @if($errors->has('neighborhood'))
+                                    <span class="text-danger">
+                                        <strong>{{ $errors->first('neighborhood') }}</strong>
+                                    </span>
+                                @endif
                             </div>
                         </div>
                         <div class="col-md-4">
                             <div class="form-group mb-2">
                                 <label for="zip">CEP</label>
-                                <input type="text" class="form-control" id="zip" name="postal_code" placeholder="Código postal" pattern="\d*" required/>
+                                <input type="text" class="form-control {{ $errors->has('postal_code') ? 'border border-danger' : 'border border-primary' }}" id="zip" name="postal_code" placeholder="Código postal" pattern="\d*" required/>
+                                @if($errors->has('postal_code'))
+                                    <span class="text-danger">
+                                        <strong>{{ $errors->first('postal_code') }}</strong>
+                                    </span>
+                                @endif
                             </div>
                         </div>
                     </div>
@@ -87,13 +136,23 @@
                         <div class="col-md-6">
                             <div class="form-group mb-2">
                                 <label for="city">Cidade</label>
-                                <input type="text" class="form-control" id="city" name="city" placeholder="Cidade" require/>
+                                <input type="text" class="form-control {{ $errors->has('city') ? 'border border-danger' : 'border border-primary' }}" id="city" name="city" placeholder="Cidade" require/>
+                                @if($errors->has('city'))
+                                    <span class="text-danger">
+                                        <strong>{{ $errors->first('city') }}</strong>
+                                    </span>
+                                @endif
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group mb-2">
                                 <label for="state">Estado</label>
-                                <input type="text" class="form-control" id="state" name="state" placeholder="Estado" required/>
+                                <input type="text" class="form-control {{ $errors->has('state') ? 'border border-danger' : 'border border-primary' }}" id="state" name="state" placeholder="Estado" required/>
+                                @if($errors->has('state'))
+                                    <span class="text-danger">
+                                        <strong>{{ $errors->first('state') }}</strong>
+                                    </span>
+                                @endif
                             </div>
                         </div>
                     </div>
