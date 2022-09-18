da6cd6c18c2ae200acc6469b137cd1f15e66ca7e
Elivandro <Elivandro@hotmail.com.br>
2022-09-18 15:32:13 -0400

    Authenticate routes added

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ImovelController;
+use Illuminate\Support\Facades\Auth;
 
+Auth::routes();
 Route::resource('imovel', ImovelController::class);
