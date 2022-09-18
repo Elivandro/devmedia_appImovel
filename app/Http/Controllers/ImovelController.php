0fdce6842388fb1b97db6ea6f1fc9b58da47dc1b
Elivandro <Elivandro@hotmail.com.br>
2022-09-18 15:38:12 -0400

    update method added

--- app/Http/Controllers/ImovelController.php
+++ app/Http/Controllers/ImovelController.php
@@ -32,23 +32,35 @@ class ImovelController extends Controller
 
     public function show($id)
     {
-        //
+        if(!$imovel = $this->imoveis->find($id))
+            return redirect()->back()->with('message', 'Imóvel não encontrado');
+
+        return view('imoveis.show', compact('imovel'));
     }
 
     public function edit($id)
     {
-        //
+        if(!$imovel = $this->imoveis->find($id))
+            return redirect()->back()->with('message', 'Imóvel não encontrado');
+
+        return view('imoveis.update', compact('imovel'));
     }
 
     public function update(createUpdateImovelRequest $request, $id)
     {
-        //
+        if(!$imovel = $this->imoveis->find($id))
+            return redirect()->back()->with('message', 'Imóvel não encontrado');
+
+        $imovel->update($request->all());
+
+        $imoveis = $this->imoveis->paginate(5);
+        return redirect()->route('imovel.index', compact('imoveis'))->with('success', 'Imóvel editado com Sucesso.');
     }
 
     public function destroy($id)
     {
         if(!$imovel = $this->imoveis->find($id))
-            return redirect()->back()->withErrors('message', 'Imóvel não encontrado.');
+            return redirect()->back()->with('message', 'Imóvel não encontrado.');
 
         $imovel->delete();
 
