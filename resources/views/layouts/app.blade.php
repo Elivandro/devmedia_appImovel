b529b5b4d0eb7c40fe3054f24d05e4668f8aa729
Elivandro <Elivandro@hotmail.com.br>
2022-09-18 15:40:55 -0400

    flash error message added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,6 +26,16 @@
                     </div>
                 </div>
             @endif
+            @if(session()->has('message'))
+                <div class="row justify-content-center">
+                    <div class="col-md-8">
+                        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                            <strong>Atenção!</strong> {{ session()->get('message') }}
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                    </div>
+                </div>
+            @endif
             @yield('content')
         </main>
         <footer class="py-5">
