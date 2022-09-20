43c0a8132e2dd2d7ef2c33ba767e1fdd8ff58640
Elivandro <Elivandro@hotmail.com.br>
2022-09-20 06:42:15 -0400

    login/register links and after login editar links added

--- resources/views/_includes/_header.blade.php
+++ resources/views/_includes/_header.blade.php
@@ -1,23 +1,54 @@
 <section>
     <nav class="navbar navbar-expand-lg bg-light">
         <div class="container container-fluid">
-            <a class="navbar-brand" href="{{ route('imovel.index') }}">Ínicio</a>
+            <a href="{{ route('todos.imoveis') }}" class="navbar-brand active" aria-current="page">Ínicio</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
-                    <a class="nav-link active" aria-current="page" href="#">Todos</a>
                     <a class="nav-link" href="#">Apartamentos</a>
                     <a class="nav-link" href="#">Casas</a>
                     <a class="nav-link" href="#">Kitnets</a>
                     <a class="nav-link" href="#">Lotes</a>
                 </div>
-                <div class="navbar-nav">
-                    <a class="nav-link" href="{{ route('login') }}">Login</a>
-                    <a class="nav-link" href="{{ route('register') }}">Register</a>
-                </div>
-            </div>
+                <ul class="navbar-nav ms-auto">
+                    @guest
+                        @if (Route::has('login'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                        @endif
+
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                        @endif
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#">
+                                        Editar Cadastro
+                                </a>
+                                <a class="dropdown-item" href="{{ route('imovel.index') }}">
+                                        Imóveis Cadastrados
+                                </a>
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
+            </ul>
         </div>
     </nav>
 </section>
