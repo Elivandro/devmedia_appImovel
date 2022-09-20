ba5e59781c0363589144e8927d87e622f693478f
Elivandro <Elivandro@hotmail.com.br>
2022-09-20 06:38:34 -0400

    routes to delete and detail added

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ImovelController;
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Auth;
 
 Auth::routes();
-Route::resource('imovel', ImovelController::class);
+Route::get('/', [PageController::class, 'index'])->name('todos.imoveis');
+Route::get('/show/{id}', [PageController::class, 'show'])->name('detail.imoveis');
+
+Route::get('/imovel/delete/{id}', [ImovelController::class, 'delete'])->name('imovel.delete');
+Route::resource('imovel', ImovelController::class)->middleware('auth');
