ad9f6803bf3711724811cf299e0622b563603123
Elivandro <Elivandro@hotmail.com.br>
2022-09-20 06:33:00 -0400

    detail imovel view added

---
+++ resources/views/pages/show.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.app')
+@section('title', 'Detalhar imóvel')
+
+@section('content')
+    <section>
+        <div class="card card-default">
+            <div class="card-body">
+                <div class="row justify-content-evenly mx-auto">
+                    <div class="card border-0" style="width: 49.2%;">
+                        <div class="card-header">
+                            <h4>Sobre o imóvel</h4>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            @switch($imovel->purpose)
+                                @case(1)
+                                    <li class="list-group-item">Propósito: Venda</li>
+                                    @break
+                                @case(2)
+                                    <li class="list-group-item">Locação</li>
+                                    @break
+                                @default
+                                    <li class="list-group-item">Propósito: Não encontrado</li>
+                            @endswitch
+                            <li class="list-group-item">Preço: {{ formatMoney($imovel->price) }}</li>
+                            <li class="list-group-item">Quantidade de quartos: {{ $imovel->roomQty }}</li>
+                            @switch($imovel->type)
+                                @case(1)
+                                    <li class="list-group-item">Tipo: Apartamento</li>
+                                    @break
+                                @case(2)
+                                    <li class="list-group-item">Tipo: Casa</li>
+                                    @break
+                                @case(3)
+                                    <li class="list-group-item">Tipo: Kitnet</li>
+                                    @break
+                                @case(4)
+                                    <li class="list-group-item">Tipo: Lote</li>
+                                    @break
+                                @default
+                                    <li class="list-group-item">Tipo: Não encontrado</li>
+                                @endswitch
+                                <li class="list-group-item">{{ $imovel->description }}</li>                                        
+                        </ul>
+                    </div>
+                    <div class="card border-0" style="width: 49.2%;">
+                        <div class="card-header">
+                            <h4>Endereço</h4>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Logradouro: {{ $imovel->address }}</li>
+                            <li class="list-group-item">Número:{{ $imovel->number }}</li>
+                            <li class="list-group-item">Bairro: {{ $imovel->neighborhood }}</li>
+                            <li class="list-group-item">CEP: {{ formatZipCode($imovel->postal_code) }}</li>
+                            <li class="list-group-item">Cidade: {{ $imovel->city }}</li>
+                            <li class="list-group-item">Estado: {{ $imovel->state }}</li>
+                        </ul>
+                        <div class="card-footer">
+                            <li class="list-group-item">Criação: {{ formatDateTime($imovel->created_at) }}</li>
+                            <li class="list-group-item">Ultima atualização: {{ formatDateTime($imovel->updated_at) }}</li>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
