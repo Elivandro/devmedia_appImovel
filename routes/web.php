9aa9768f5ebb05c269035e39042419eb34beb5ee
Elivandro <Elivandro@hotmail.com.br>
2022-09-22 22:06:35 -0400

    new routes created

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,9 @@ use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Auth;
 
 Auth::routes();
-Route::get('/', [PageController::class, 'index'])->name('todos.imoveis');
-Route::get('/show/{id}', [PageController::class, 'show'])->name('detail.imoveis');
+Route::get('/', [PageController::class, 'index'])->name('page.index');
+Route::get('/show/{id}/imovel', [PageController::class, 'show'])->name('page.show');
+Route::get('/{type}/imoveis', [PageController::class, 'filter'])->name('page.filter');
 
 Route::get('/imovel/delete/{id}', [ImovelController::class, 'delete'])->name('imovel.delete');
-Route::resource('imovel', ImovelController::class)->middleware('auth');
+Route::resource('/imovel', ImovelController::class)->middleware('auth');
