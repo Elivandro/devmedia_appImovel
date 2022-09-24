18f307b4a9fbd1c9f505cc332745cc3b2fbd4f38
Elivandro <Elivandro@hotmail.com.br>
2022-09-24 14:15:52 -0400

    account index route added

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\ImovelController;
 use App\Http\Controllers\PageController;
-use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\AccountController;
 
 Auth::routes();
 Route::get('/', [PageController::class, 'index'])->name('page.index');
 Route::get('/show/{id}/imovel', [PageController::class, 'show'])->name('page.show');
 Route::get('/{type}/imoveis', [PageController::class, 'filter'])->name('page.filter');
+
+Route::get('/user', [AccountController::class, 'index'])->name('user.index')->middleware('auth');
 
 Route::get('/imovel/delete/{id}', [ImovelController::class, 'delete'])->name('imovel.delete');
 Route::resource('/imovel', ImovelController::class)->middleware('auth');
